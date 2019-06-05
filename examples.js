c63f323c123fb9e9e9de785eb005d39fe1bfd9f6
Henrique Breim <breim@protonmail.ch>
2019-06-05 19:15:07 -0300

    Update examples.js

--- examples.js
+++ examples.js
@@ -87,3 +87,68 @@ console.log(toArray(1, 2, 3, 4));
 var d = new Date(...camposData);
 //----------------------------------------------------
 
+
+
+// Destructor operator
+// ===============================================
+
+// O destructor cria variaveis a partir de uma constante. Você "pega" ela da constante.
+
+const pessoa = {
+  name: 'Henrique',
+  age: 27,
+  mensagem() {
+    console.log('Olá meu nome é ' + this.name);
+  }
+}
+
+let { name, age } = pessoa;
+console.log(name, age);
+//----------------------------------------------------
+
+
+
+
+// Async & Promisse
+// ===============================================
+
+// Como era feito antes do async?
+  setTimeout(() => {
+    somar(1, 2)
+    console.log('Vou aparecer na sua tela depois de 2 segundos');
+  }, 2000);
+
+// 
+// A promisse tem dois tipos de retorno .then , .cath para exception ES5
+// async await - async coloca no cabeçalho. await coloca na frente da função. await retorna uma promisse.
+// Geralmente usam quando precisam consultar algo de terceiros, por exemplo consultar uma API.
+//
+// A promisse tem dois status: pending e undefinied, caso ela ocorra com sucesso retorna state fulfilled(result: valor) ou erro como state(rejected e result: error) 
+
+let promise = () => new Promise(function(resolve, reject) {
+  // the function is executed automatically when the promise is constructed
+  console.log('test');
+  // after 1 second signal that the job is done with the result "done"
+  //setTimeout(() => resolve(20), 2000);
+});
+
+
+(async () => {
+  const result = await promise()
+  const result1 = await promise()
+  // promise().then(d => {
+  //   console.log(d)
+  // })
+  //console.log(result)
+  //console.log(1000)
+})()
+
+console.log('adsdsaas')
+
+const [pro1, pro2] = await Promise.all([
+  new Promise(),
+  new Promise()
+])
+//----------------------------------------------------
+
+
